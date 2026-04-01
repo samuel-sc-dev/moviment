69048022d182cf49b9d333dcd261ffe78084c796
samuel-sc-dev <samuelsc.dev@gmail.com>
2026-04-01 00:10:18 -0300

    01040010

---
+++ script.js
@@ -0,0 +1,9 @@
+function clicouSim() {
+    alert("🚀 Bem-vindo à viagem!");
+    // aqui você pode redirecionar:
+    // window.location.href = "https://seusite.com";
+}
+
+function clicouNao() {
+    alert("😢 Que pena!");
+}
